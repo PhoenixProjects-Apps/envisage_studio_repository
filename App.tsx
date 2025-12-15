d03b27e4b69c23177cd8445856bf8ecf80e85d2b
PhoenixProjects-Apps <daniel@phoenixprojects.com.au>
2025-12-15 11:55:56 +1000

    Removed importmap

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react?external=react';
 import Sidebar from './components/Sidebar';
 import { ToolType, GeneratedAsset, User } from './types';
 import LayoutEditor from './components/LayoutEditor';
